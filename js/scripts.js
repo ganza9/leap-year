7307760d73c4f7d13c77dde515eed825d4ababa9
Gennaro Anzalone and Rafael Furry <gennaro@>
2017-08-16 09:05:03 -0700

    finish the leap year webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
-function clicked(event){
-  event.preventDefault();
-  var stringIn = $("input#stringIn").val();
+function leapYear(year){
+if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+  };
 
-  $("#stringOut").html(stringIn)
+$(document).ready(function(){
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#stringIn").val());
+    var result = leapYear(year);
+    $("#year").text(year);
+
+    if(!result){
+      $("#not").text("not");
+    } else {
+      $("#not").text("");
+    }
+    $("#stringOut").show();
+
+  });
 });
